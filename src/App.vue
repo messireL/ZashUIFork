8cdfdcdec3b8dd3399fa8228b5418b5b32cd4578
Zephyruso <176294927+Zephyruso@users.noreply.github.com>
2025-08-24 23:55:44 +0800

    fix: safe area padding

--- src/App.vue
+++ src/App.vue
@@ -103,7 +103,7 @@ useKeyboard()
     ref="app"
     id="app-content"
     :class="[
-      'bg-base-100 flex h-dvh w-screen overflow-x-hidden',
+      'bg-base-100 flex h-dvh w-screen overflow-hidden',
       fontClassName,
       backgroundImage &&
         `custom-background-${dashboardTransparent} custom-background bg-cover bg-center`,
