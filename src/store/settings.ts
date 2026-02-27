4689a02c1c9194512330542478456b43baeaad6e
Denis Maksimov <20766867+messireL@users.noreply.github.com>
2026-02-27 11:20:10 +0300

    Update settings.ts

--- src/store/settings.ts
+++ src/store/settings.ts
@@ -82,6 +82,7 @@ export const displayAllFeatures = useStorage('config/display-all-features', fals
 export const blurIntensity = useStorage('config/blur-intensity', 10)
 export const scrollAnimationEffect = useStorage('config/scroll-animation-effect', true)
 export const IPInfoAPI = useStorage('config/geoip-info-api', IP_INFO_API.IPSB)
+export const twoIpToken = useStorage('config/twoip-token', '')
 export const autoDisconnectIdleUDP = useStorage('config/auto-disconnect-idle-udp', false)
 export const autoDisconnectIdleUDPTime = useStorage('config/auto-disconnect-idle-udp-time', 300)
 
